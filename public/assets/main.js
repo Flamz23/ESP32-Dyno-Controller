911c9c94e63abd7488b95c20fbd8132735d13d75
Dami <59375070+Flamz23@users.noreply.github.com>
2024-02-08 23:02:03 -0500

    added file import elements and logic

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,100 +1,145 @@
+let port;
+let portInfo;
+let parsedTraceFile;
+const baud = 57600;
+
+/***********************************************************************************************************/
+
 // check web serial api availability
 if ("serial" in navigator) {
-    console.log("The web serial API is supported");
+  console.log("The web serial API is supported");
 } else {
-    alert("The web serial API is not supported in this context or browser");
+  alert("The web serial API is not supported in this context or browser");
 }
 
 // alert user when device is disconnected
 navigator.serial.addEventListener("disconnect", (e) => {
-    alert("Device disconnected");
+  alert("Device disconnected");
 });
 
-let port;
-let portInfo;
-const baud = 57600;
-
 async function connectDevice() {
-    try {
-      // Request access to the selected serial port
-      port = await navigator.serial.requestPort();
+  try {
+    // Request access to the selected serial port
+    port = await navigator.serial.requestPort();
 
-      // Open the port
-      await port.open({ baudRate: baud });
+    // Open the port
+    await port.open({ baudRate: baud });
 
-      // get vendor and product id
-      portInfo = port.getInfo();
+    // get vendor and product id
+    portInfo = port.getInfo();
 
-      alert(`Connected to Vendor: ${portInfo.usbVendorId} (${portInfo.usbProductId})`);
-    } catch (error) {
-        console.error('Error connecting to port:', error);
-    }
+    alert(
+      `Connected to Vendor: ${portInfo.usbVendorId} (${portInfo.usbProductId})`
+    );
+  } catch (error) {
+    console.error("Error connecting to port:", error);
   }
-
+}
 
 async function disconnectDevice() {
-
-}
-
-async function readSerial() {
-    try {
-        // Create a text decoder to handle received data
-        const decoder = new TextDecoderStream('utf-8');
-
-        // Create a readable stream from the serial port
-        const readableStreamClosed = port.readable.pipeTo(decoder.writable);
-
-        // Get a reference to the readable stream
-        const readableStream = decoder.readable;
-
-        // Start reading data from the stream
-        const reader = readableStream.getReader();
-
-        // Continuously read data from the serial port
-        while (true) {
-            const { value, done } = await reader.read();
-  
-            if (done) {
-              console.log('Reader has been closed');
-              break;
-            }
-  
-            // Display the received data
-            //document.getElementById('output').innerText += value + '\n';
-            console.log(value + '\n')
-          }
-    } catch (error) {
-        
+  try {
+    if (port) {
+      // Close the serial port
+      await port.close();
+      console.log("Serial connection closed.");
+    } else {
+      console.warn("No active serial connection.");
     }
+  } catch (error) {
+    console.error("Error closing serial connection:", error);
+  }
 }
 
-function parseMessage() {
-    
+/***********************************************************************************************************/
+
+async function readSerialMessage() {
+  try {
+    // Create a text decoder to handle received data
+    const decoder = new TextDecoderStream("utf-8");
+
+    // Create a readable stream from the serial port
+    const readableStreamClosed = port.readable.pipeTo(decoder.writable);
+
+    // Get a reference to the readable stream
+    const readableStream = decoder.readable;
+
+    // Start reading data from the stream
+    const reader = readableStream.getReader();
+
+    // read data from the serial port and parse
+    while (true) {
+      const { value, done } = await reader.read();
+      if (done) {
+        console.log("Reader has been closed");
+        break;
+      }
+
+      // Display the received data
+      //document.getElementById('output').innerText += value + '\n';
+      console.log(value + "\n");
+    }
+  } catch (error) {}
 }
 
+function parseSerialMessage() {}
 
+function sendSerialMessage() {}
 
+/***********************************************************************************************************/
 
+function importTraceFile() {
+  // Get the file input element
+  var fileInput = document.getElementById("csv-file-picker");
 
+  // Check if a file is selected
+  if (fileInput.files.length > 0) {
+    var file = fileInput.files[0];
+    var reader = new FileReader();
 
+    reader.onload = function (e) {
+      // e.target.result contains the content of the file
+      var csvContent = e.target.result;
 
+      // Parse the CSV content
+      var parsedTraceFile = parseTraceFile(csvContent);
 
+      // console.log(csvContent);
+      // send to MCU
+    };
 
+    // Read the file as text
+    reader.readAsText(file);
+  } else {
+    alert("Please select a CSV file.");
+  }
+}
 
+function parseTraceFile(data) {
+  // Split CSV content into rows
+  var rows = data.split("\n");
 
+  // Create an array to hold the parsed data
+  var data = [];
 
+  // Loop through rows and split each row into columns
+  for (var i = 0; i < rows.length; i++) {
+    var columns = rows[i].split(",");
+    data.push(columns);
+  }
 
+  return data;
+}
 
+/***********************************************************************************************************/
 
-
-
+/***********************************************************************************************************/
 
 // google.charts.load('current', { 'packages': ['corechart'] }); // Load the Visualization API
 // google.charts.setOnLoadCallback(drawChart); // API load callback
 
 // function UpdateData(value, data, chart, options) {
 //     var today = new Date();
-//     data.addRow([`${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`, value]); // Add to the data   
+//     data.addRow([`${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`, value]); // Add to the data
 //     chart.draw(data, options); // update chart
 
 //     // Stop adding data after 10 points
