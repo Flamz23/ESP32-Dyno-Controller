6212bd08649f1fea3ea1350542f295684c6272bc
Dami <59375070+Flamz23@users.noreply.github.com>
2024-02-22 01:29:32 -0500

    Added bme280 libraries and test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,46 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+Adafruit_BME280 bme; // I2C
+
+unsigned long delayTime;
+
+
+void printValues() {
+    Serial.print("temp:");
+    Serial.print(bme.readTemperature());
+    Serial.print("*C, ");
+
+    Serial.print("pressure:");
+    Serial.print(bme.readPressure()/100.0F);
+    Serial.print("hPa, ");
+
+    Serial.print("humidity:");
+    Serial.print(bme.readHumidity());
+    Serial.println("%");
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  bool rslt;
+    rslt = bme.begin();  
+    if (!rslt) {
+        Serial.println("Init Fail,Please Check your address or the wire you connected!!!");
+        while (1);
+    }
+
+    Serial.println("Init Success");
+    Serial.println("Temperature           Pressure             Humidity");
+    delayTime = 40;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  printValues();
+  delay(delayTime);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
