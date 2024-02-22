1c068db570f1e552bf6e8dff0f43fea5e7c34dd5
Dami <59375070+Flamz23@users.noreply.github.com>
2024-02-22 16:25:24 -0500

    Added rpm sensor code and interrupt method

--- src/main.cpp
+++ src/main.cpp
@@ -5,42 +5,84 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
+/****************** BME280 ********************/
 Adafruit_BME280 bme; // I2C
-
 unsigned long delayTime;
+/****************** BME280 ********************/
+
+/****************** RPM Sensor ********************/
+volatile unsigned long previousMillis = 0;
+volatile unsigned long pulseDuration = 0;
+float pulsePeriodMs = 0;
+float pulseRPM = 0;
+volatile boolean fallingEdgeDetected = false;
+const int interruptPin = 2;
+/****************** RPM Sensor ********************/
 
 
-void printValues() {
-    Serial.print("temp:");
-    Serial.print(bme.readTemperature());
-    Serial.print("*C, ");
+void printValues()
+{
+  Serial.print("temp:");
+  Serial.print(bme.readTemperature());
+  Serial.print("*C, ");
 
-    Serial.print("pressure:");
-    Serial.print(bme.readPressure()/100.0F);
-    Serial.print("hPa, ");
+  Serial.print("pressure:");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.print("hPa, ");
 
-    Serial.print("humidity:");
-    Serial.print(bme.readHumidity());
-    Serial.println("%");
+  Serial.print("humidity:");
+  Serial.print(bme.readHumidity());
+  Serial.print("%");
+
+  Serial.print(", PDuration:");
+  Serial.print(pulseDuration);
+
+  Serial.print(", PPeriod:");
+  Serial.print(pulsePeriodMs);
+
+  Serial.print(", rpm:");
+  Serial.println(pulseRPM);
 }
 
-void setup() {
+void fallInterrupt() {
+  unsigned long currentMillis = micros(); // Use micros() for microsecond precision
+  if (previousMillis != 0) {
+    pulseDuration = currentMillis - previousMillis;
+    fallingEdgeDetected = true;
+  }
+  previousMillis = currentMillis;
+  pulsePeriodMs = pulseDuration / 1000.0; // Convert microseconds to milliseconds
+  pulseRPM = (1.0 /((pulsePeriodMs / 1000.0) * 8.0)) * 60.0; // 8div; convert to seconds, freq then per minutes
+}
+
+
+
+void setup()
+{
   Serial.begin(115200);
 
-  bool rslt;
-    rslt = bme.begin();  
-    if (!rslt) {
-        Serial.println("Init Fail,Please Check your address or the wire you connected!!!");
-        while (1);
-    }
+  /****************** BME280 ********************/
+  bool result;
+  result = bme.begin();
+  if (!result)
+  {
+    Serial.println("Init Fail,Please Check your address or the wire you connected!!!");
+    while (1)
+      ;
+  }
+  Serial.println("BME Init Success");
+  delayTime = 40;
+  /****************** BME280 ********************/
 
-    Serial.println("Init Success");
-    Serial.println("Temperature           Pressure             Humidity");
-    delayTime = 40;
+  /****************** RPM Sensor ********************/
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), fallInterrupt, FALLING);
+  /****************** RPM Sensor ********************/
+
 }
 
-void loop() {
+void loop()
+{
   printValues();
   delay(delayTime);
 }
-
